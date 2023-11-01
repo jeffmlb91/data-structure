e5878963c21ffec08c19ff759027850912657569
jeffmlb91 <jeffmlb91@gmail.com>
2023-11-01 11:41:21 -0400

    Learned first rule of O(n) Drop constant

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logItems(n) {
         console.log(i);
     }
 }
-logItems(10);
+// logItems(10);
 
 let arrayOfNumbers = [1,2,3,4,5,6];
 
@@ -24,4 +24,21 @@ function increaseNum() {
         console.log("This is ", i)
     }
 }
-increaseNum()
+// increaseNum()
+
+{/**
+First Rule: DROP CONSTANTS
+O(n) Drop Constants. 
+In the following the first forloop operation is 0(n) and the second is O(n)
+The function logItems2 has 2 operations n + n = O(2n) which equals O(n)
+we dropped the constant
+*/}
+function logItems2(n) {
+    for (let i = 0; i < n; i ++) {
+        console.log("first operation", i)
+    }
+    for ( let j = 0; j < n; j ++ ) {
+        console.log("second operation", j)
+    }
+}
+logItems2(3)
